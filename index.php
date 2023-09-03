1f0200a0d4c13b257c47d0659f04553d8a22203d
oyerohabib <oyerohabib@yahoo.com>
2023-09-03 23:36:19 +0100

    feat: created the index.php as an entry point for the app

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__ . '/app/Controllers/CapsuleController.php';
+require_once __DIR__ . '/app/Enum/ActionEnum.php';
+require_once  __DIR__ . '/app/Helpers/function.php';
+
+$capsuleController = new CapsuleController();
+
+if ($_SERVER["REQUEST_METHOD"] == ActionEnum::GET->name && $_SERVER['REQUEST_URI'] == '/api/get_capsules') {
+    $capsuleController->getCapsules();
+}
+
+$requestPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$pattern = '/^\/api\/get_capsules\/([A-Za-z0-9]+)$/';
+
+if ($_SERVER["REQUEST_METHOD"] == ActionEnum::GET->name && preg_match($pattern, $requestPath, $matches)) {
+    $serial = $matches[1];
+    $capsuleController->getOneCapsule($serial);
+}
